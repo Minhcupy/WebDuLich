5a745efc5b86058fafd9bea453463f569686dd80
Minhcupy <tqmminh2004tqm@gmail.com>
2025-08-27 21:38:53 +0700

    first commit

---
+++ src/main/java/com/trinhquangminh/webdulich/WebDuLichApplication.java
@@ -0,0 +1,13 @@
+package com.trinhquangminh.webdulich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDuLichApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDuLichApplication.class, args);
+    }
+
+}
